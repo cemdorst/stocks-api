7eabda572f2e379914377fdfb060fc8315ef7982
Cae <cemdorst@gmail.com>
2020-08-26 15:03:13 -0300

    Fix typo in models

--- models/stock.go
+++ models/stock.go
@@ -5,8 +5,7 @@ import "gopkg.in/mgo.v2/bson"
 // Represents a stock, we uses bson keyword to tell the mgo driver how to name
 // the properties in mongodb document
 type Stock struct {
-	ID          bson.ObjectId `bson:"_id" json:"id"`
 	Symbol      string        `bson:"symbol" json:"symbol"`
 	Sector      string        `bson:"sector" json:"sector"`
-	Subsector   string        `bson:"subsector" json:"subsector"`
+	Subsector   string        `bson:"subSector" json:"subsector"`
 }
