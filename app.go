da5bfe113f2f949c610ba53c12e2fac8d257513d
Cae <cemdorst@gmail.com>
2020-08-26 10:35:08 -0300

    Point packages to new repo

--- app.go
+++ app.go
@@ -8,9 +8,9 @@ import (
 	"gopkg.in/mgo.v2/bson"
 
 	"github.com/gorilla/mux"
-	. "github.com/mlabouardy/movies-restapi/config"
-	. "github.com/mlabouardy/movies-restapi/dao"
-	. "github.com/mlabouardy/movies-restapi/models"
+	. "github.com/cemdorst/stocks-api/config"
+	. "github.com/cemdorst/stocks-api/dao"
+	. "github.com/cemdorst/stocks-api/models"
 )
 
 var config = Config{}
